5b8e4d63baaeb4f2d7bd0dd7e3a094ef099e2aed
Paul Howe <pahowe@gmail.com>
2011-08-26 15:33:43 -0700

    organizing

---
+++ src/1-5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main(){
+
+  int lower, upper, step;
+  float fahr, celsius;
+
+  printf("Fahrenheit -> Celsius conversion table\n");
+  // for(fahr=0;fahr <= 300;fahr += 20){
+  for(fahr=0;fahr <= 300;fahr += 20){
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+  }
+}
