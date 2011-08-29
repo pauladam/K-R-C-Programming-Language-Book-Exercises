1484741513d74eea0347456526ab8c8e6f652eb8
Paul Howe <pahowe@gmail.com>
2011-08-29 13:40:24 -0700

    exercises 1-{12, 13}

--- src/1-12.c
+++ src/1-12.c
@@ -1,5 +1,24 @@
+// Exercise 1-12
+// ================================================================
+// Write a program that prints its input one word per line.
+
 #include <stdio.h>
+#include <stdbool.h>
 
-main(){
+main(int argc, char *argv){
 
+  int c;
+  bool afternewline = false;
+
+  while((c = getchar()) != EOF){
+    if((c == ' ' || c == '\n') && !afternewline){
+      printf("\n");
+      afternewline = true;
+    }else if(c != ' ' && c != '\n'){
+      putchar(c);
+      afternewline = false;
+    }
+  }
+
+  return 0;
 }
--- src/1-13.c
+++ src/1-13.c
@@ -1,5 +1,75 @@
+// Exercise 1-13
+// ================================================================
+// Write a program to print a histogram of the lengths of words in
+// its input. It is easy to draw the histogram with the bars horizontal; a
+// vertical orientation is more challenging.
+
+// Example output
+// src λ: cat /Users/paul/Desktop/words | ../bin/1-13 
+// 1: *********************************************************************
+// 2: ************************************************************
+// 3: *********************************************************************
+// 4: ***********************************
+// 5: ******************************************************
+// 6: ********************************************************
+// 7: **************************************
+// 8: ***************************
+// 9: *************************************
+// 10: ******************
+// 11: *****
+// 12: **
+
 #include <stdio.h>
+#include <stdbool.h>
 
-main(){
+#define MAX_WORD_LENGTH 80
 
+int main(char argc, char *argv){
+
+  int wlength[MAX_WORD_LENGTH]; 
+  int c = 0, i = 0, j = 0, cwordlength = 0;
+  int lastcharword = false, curcharword = false;
+
+  for(i=0;i<MAX_WORD_LENGTH;i++)
+    wlength[i] = 0;
+
+  // a : 97, A : 65,  z : 122, Z: 90
+
+  while((c = getchar()) != EOF){
+
+    if(c >= 'A' && c <= 'z'){ // word character
+      
+      if(lastcharword && curcharword)
+        cwordlength++;
+
+      curcharword = true;
+
+    }else{
+
+      // update table
+      wlength[cwordlength]++;
+
+      cwordlength = 0;
+      curcharword = false;
+    }
+
+    lastcharword = curcharword;
+
+  }
+
+  // print histogram
+  for(i=1;i<80;i++){
+
+    if(wlength[i] > 0){
+
+      printf("%d: ", i);
+
+      for(j=0; j<wlength[i]; j++)
+        putchar('*');
+      printf("\n");
+
+    }
+  }
+
+  return 0;
 }
